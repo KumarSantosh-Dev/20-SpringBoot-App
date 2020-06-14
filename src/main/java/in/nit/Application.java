2ee2b492af828d70392ace7142f9b2778d023a56
KumarSantosh-Dev <sksahoo19970520@gmail.com>
2020-06-14 06:20:35 +0530

    CSL 200:feature-200 comiited

--- src/main/java/in/nit/Application.java
+++ src/main/java/in/nit/Application.java
@@ -9,5 +9,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	//HIS-200 related changes
+	public void doProcess(){
+	       //logic 
+	}
 
 }
